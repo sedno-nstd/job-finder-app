7cb4215be5b997b963e6927ecd1daf7cdaa1af8a
kirill paley <kirillpaley2008@gmail.com>
2026-03-02 03:00:34 +0200

    chore: remove legacy pages, hooks and unused actions

--- src/app/(dashboard)/onBoarding/page.tsx
+++
@@ -1,9 +0,0 @@
-import { OnBoarding } from "@/src/components/onboarding/OnBoarding";
-
-export default function Page() {
-  return (
-    <div className="w-full h-full">
-      <OnBoarding />
-    </div>
-  );
-}
--- src/app/(dashboard)/recommendations/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Recomande } from "@/src/components/recommendations/Recomande";
-
-export default function Page() {
-  return (
-    <div className="w-full  max-w-[928px]  flex justify-center items-center pt-10">
-      <Recomande />
-    </div>
-  );
-}
